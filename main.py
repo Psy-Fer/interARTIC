866e0b7c3db86e96fa915fcc2c269f9d7ab17e34
Alyne Gollon <alynegollon@gmail.com>
2020-06-24 16:20:30 +1000

    Added output page with basic functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,22 @@ def home():
 @app.route("/about")
 def about():
 	return render_template("about.html")
-    
+
+@app.route("/output")
+def output():
+	return render_template("output.html", job_name='Job 1', output_folder='/my_ouput_folder')
+
+@app.route("/output", methods = ["POST"])
+def outputFormHandler():
+    plots = []
+    if request.form.get('barplot') == "yes":
+        plots.append("barplot")
+    if request.form.get('boxplot') == "yes":
+        plots.append("boxplot")
+    if request.form['submit_button'] == 'Preview':
+        return render_template("output.html", job_name='Job 1', output_folder='/my_ouput_folder', preview_plots=plots)
+    if request.form['submit_button'] == 'Download':
+        return render_template("output.html", job_name='Job 1', output_folder='/my_ouput_folder', download_plots=plots)
+
 if __name__ == "__main__":
     app.run(debug=True)
