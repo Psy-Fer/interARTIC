9571c643cf2c0b25b8bdbf424864b3cd18e5004a
Alyne Gollon <alynegollon@gmail.com>
2020-06-26 11:41:14 +1000

    fixed up css and implemented get request utilising query for job name and output folder

--- main.py
+++ main.py
@@ -10,21 +10,26 @@ def home():
 def about():
 	return render_template("about.html")
 
-@app.route("/output")
+#not sure if this should be a get method
+@app.route("/output", methods = ["GET", "POST"])
 def output():
-	return render_template("output.html", job_name='Job 1', output_folder='/my_ouput_folder')
-
-@app.route("/output", methods = ["POST"])
-def outputFormHandler():
-    plots = []
-    if request.form.get('barplot') == "yes":
-        plots.append("barplot")
-    if request.form.get('boxplot') == "yes":
-        plots.append("boxplot")
-    if request.form['submit_button'] == 'Preview':
-        return render_template("output.html", job_name='Job 1', output_folder='/my_ouput_folder', preview_plots=plots)
-    if request.form['submit_button'] == 'Download':
-        return render_template("output.html", job_name='Job 1', output_folder='/my_ouput_folder', download_plots=plots)
+    job_name = request.args.get('job_name')
+    output_folder = request.args.get('output_folder')
+#@app.route("/output", methods = ["POST"])
+#def outputFormHandler():
+    if request.method == "POST":
+        plots = []
+        if request.form.get('barplot') == "yes":
+            plots.append("barplot")
+        if request.form.get('boxplot') == "yes":
+            plots.append("boxplot")
+        if not plots:
+            plots = "Nothing selected."
+        if request.form['submit_button'] == 'Preview':
+            return render_template("output.html", job_name=job_name, output_folder=output_folder, preview_plots=plots)
+        if request.form['submit_button'] == 'Download':
+            return render_template("output.html", job_name=job_name, output_folder=output_folder, download_plots=plots)
+    return render_template("output.html", job_name=job_name, output_folder=output_folder)
 
 @app.route("/progress")
 def progress():
@@ -33,6 +38,6 @@ def progress():
 @app.route("/parameters")
 def parameters():
 	return render_template("parameters.html")
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
