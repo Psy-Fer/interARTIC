8d597cfcc62b26909839ca52bef0f4f16636701b
rachelbj <rbowen-james@ccia.org.au>
2020-06-26 12:28:05 +1000

    Updated progress page, added routes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template  
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -9,6 +10,15 @@ def home():
 @app.route("/about")
 def about():
 	return render_template("about.html")
-    
+
+@app.route("/newJob")
+def newJob():
+	#return render_template("newJob.html")
+    return "Create new job"
+
+@app.route("/progress/<string:job_id>")
+def progress(job_id):
+	return render_template("progress.html", job_id=job_id)
+
 if __name__ == "__main__":
     app.run(debug=True)
