eb7bd3d0872e31900cde116a22e940b68972a44b
Chiara <chichibabe98@gmail.com>
2020-07-03 12:49:30 +1000

    gather cmd medaka

--- main.py
+++ main.py
@@ -37,39 +37,53 @@ def parameters():
         sampleName = sampleName.replace(" ", "_")
 
         #these are for gather cmd
-        #minLength = request.form['minLength']
-        #maxLength = request.form['maxLength']
+        minLength = request.form.get('minLength')
+        maxLength = request.form.get('maxLength')
         #csv_file = request.form['csvFile']
 
-        #only doing minion cmd for first sprint
-        #below is a sample cmd
-        minion_cmd = "artic minion --minimap2 --medaka --normalise 200 --threads 4 --scheme-directory /Users/iggygetout/Documents/binf6111_project/artic-ncov2019/primer_schemes --read-file /Users/iggygetout/Documents/binf6111_project/data/SP1-raw/SP1-mapped.fastq nCoV-2019/V1 sample_name"
+        gather_cmd = ""
+        dem_cmd = ""
+        minion_cmd = ""
 
         #if nanopolish selected
         if request.form.get('pipeline') == "nanopolish":
-            #run nanopolish cmd - to be fixed
-            minion_cmd = "blah"
+            #construct cmds
+            minion_cmd = "echo 'no command for nanopolish yet'"
         #if medaka selected
         elif request.form.get('pipeline') == "medaka":
-            #run medaka cmd
+            #construct cmds
+            gather_cmd = "artic gather --min-length " + minLength + " --max-length " + maxLength + " --prefix " + sampleName + " --directory " + input_folder +" --no-fast5s"
             minion_cmd = "artic minion --minimap2 --medaka --normalise " + normalise + " --threads " + numThreads + " --scheme-directory " + scheme_dir + " --read-file " + read_file + " " + primer_scheme + " \"" + sampleName + "\""
         #if both nano and medaka are selected
         elif request.form.get('pipeline') == "both":
-            #will be nanopolish then medaka cmds separated by ';'
-            minion_cmd = "blah"
+            #construct commands joined together
+            minion_cmd = "echo 'no command for nanopolish yet'"
         
         #if user agrees output can override files with the same name in output folder
         if request.form.get('overRideData'):
             overRide = True
 
         #need to encode - '/' in file path screws with url
+        gather_cmd = base64.b64encode(gather_cmd.encode())
         output_folder = base64.b64encode(output_folder.encode())
         minion_cmd = base64.b64encode(minion_cmd.encode())
 
         #return render_template("progress.html", min_cmd = minion_cmd)
-        return redirect(url_for('progress', min_cmd = minion_cmd, sample_name = sampleName, output_folder = output_folder))
+        return redirect(url_for('progress', gather_cmd = gather_cmd, min_cmd = minion_cmd, sample_name = sampleName, output_folder = output_folder))
     return render_template("parameters.html")
 
+@app.route("/progress/<gather_cmd>/<min_cmd>/<sample_name>/<output_folder>", methods = ["GET", "POST"])
+def progress(gather_cmd, min_cmd, sample_name, output_folder):
+    #decode
+    gather_cmd = base64.b64decode(gather_cmd).decode()
+    output_folder = base64.b64decode(output_folder).decode()
+    min_cmd = base64.b64decode(min_cmd).decode()
+    #run minion cmd
+    os.system(gather_cmd)
+    os.system(min_cmd)
+    #move output files into output folder
+    os.system('mv ' + sample_name + '* ' + output_folder)
+    return render_template("progress.html")
 
 #not sure if this should be a get method
 @app.route("/output", methods = ["GET", "POST"])
@@ -90,16 +104,6 @@ def output():
             return render_template("output.html", job_name=job_name, output_folder=output_folder, download_plots=plots)
     return render_template("output.html", job_name=job_name, output_folder=output_folder)
 
-@app.route("/progress/<min_cmd>/<sample_name>/<output_folder>", methods = ["GET", "POST"])
-def progress(min_cmd, sample_name, output_folder):
-    #decode
-    output_folder = base64.b64decode(output_folder).decode()
-    min_cmd = base64.b64decode(min_cmd).decode()
-    #run minion cmd
-    os.system(min_cmd)
-    #move output files into output folder
-    os.system('mv ' + sample_name + '* ' + output_folder)
-    return render_template("progress.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
