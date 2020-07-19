70e7220634a15cb5b737facd3288b4bb3472f7e1
Thanh Nguyen <thanh.nguyen4@student.unsw.edu.au>
2020-07-20 04:16:20 +1000

    activating the conda env && taking the readfile requirement off

--- run.sh
+++ run.sh
@@ -13,17 +13,17 @@ term=$1
 if [ $term == "xterm" ]
 then
     xterm -title "Redis Server" -e "bash $script_full_path/run-redis.sh" 
-    xterm -title "Celery" -e "cd $script_full_path; celery worker -A main.celery -loglevel=info" 
+    xterm -title "Celery" -e "cd $script_full_path; conda activate artic-ncov2019;celery worker -A main.celery -loglevel=info" 
     xterm -title "Web App" -e "python3 $script_full_path/main.py"
 elif [ $term == "macos" ]
 then
     osascript -e 'tell application "Terminal" to do script "bash '$script_full_path'/run-redis.sh"' 
-    osascript -e 'tell application "Terminal" to do script "cd '$script_full_path';celery worker -A main.celery --loglevel=info"' 
+    osascript -e 'tell application "Terminal" to do script "cd '$script_full_path';conda activate artic-ncov2019;celery worker -A main.celery --loglevel=info"' 
     osascript -e 'tell application "Terminal" to do script "python3 '$script_full_path'/main.py"' 
 elif [ $term == "konsole" ]
 then
     konsole -e "bash $script_full_path/run-redis.sh"
-    konsole -e "cd $script_full_path; celery worker -A main.celery -loglevel=info" 
+    konsole -e "cd $script_full_path; conda activate artic-ncov2019;celery worker -A main.celery -loglevel=info" 
     konsole -e "python3 $script_full_path/main.py"
 elif [ $term == "git" ]
 then
