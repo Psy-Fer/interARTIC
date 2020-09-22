4f6880f60f23ce875914e57a2dfc690e02a5ef87
Chiara <chichibabe98@gmail.com>
2020-09-23 00:01:26 +1000

    barcode files generated in barcode folders

--- src/job.py
+++ src/job.py
@@ -200,7 +200,6 @@ class Job:
             # if medaka is chosen
             if self._pipeline == "medaka":
                 #open csv file
-                # print("this is THE filepath:" + self._csv_file + "\n")
                 with open(self._csv_file,'rt')as f:
                     data = csv.reader(f)
                     for row in data:
@@ -208,11 +207,12 @@ class Job:
                         barcode = row[1]
                         #create directory for barcode with naming system
                         dir_path = self._output_folder + "/" + self._primer_type + "_" + sample_name + "_" + self._run_name + "_" + barcode + "_" + self._pipeline
+                        #make directory
                         minion_cmd = minion_cmd + "; mkdir " + dir_path
+                        #move fastq_pass file into folder 
+                        minion_cmd = minion_cmd + "; mv " + self._output_folder + "/" + self._job_name + "_fastq_pass-" + barcode + ".fastq " + dir_path
                         #append minion cmd in barcode directory
-                        minion_cmd = minion_cmd + "; artic minion --minimap2 --medaka --normalise " + self._normalise + " --threads " + self._num_threads + " --scheme-directory " + self._primer_scheme_dir + " --read-file ./" + self._job_name + "_fastq_pass-" + barcode + ".fastq " + self._primer_scheme + " " + self._job_name + "_" + barcode + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
-                        #output goes into current directory so this moves all output files to correct folder
-                        minion_cmd = minion_cmd + "; mv " + self._job_name + "_" + barcode + "* " + dir_path
+                        minion_cmd = minion_cmd + "; cd " + dir_path + "; artic minion --minimap2 --medaka --normalise " + self._normalise + " --threads " + self._num_threads + " --scheme-directory " + self._primer_scheme_dir + " --read-file ./" + self._job_name + "_fastq_pass-" + barcode + ".fastq " + self._primer_scheme + " " + self._job_name + "_" + barcode + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
 
             elif self._pipeline == "nanopolish":
                 #open csv file
@@ -223,12 +223,13 @@ class Job:
                         barcode = row[1]
                         #create directory for barcode with naming system
                         dir_path = self._output_folder + "/" + self._primer_type + "_" + sample_name + "_" + self._run_name + "_" + barcode + "_" + self._pipeline
+                        #make directory
                         minion_cmd = minion_cmd + "; mkdir " + dir_path
+                        #move fastq_pass file into folder 
+                        minion_cmd = minion_cmd + "; mv " + self._output_folder + "/" + self._job_name + "_fastq_pass-" + barcode + ".fastq " + dir_path
                         #append minion cmd in barcode directory
-                        minion_cmd = minion_cmd + "; artic minion --normalise " + self._normalise + " --threads " + self._num_threads + " --scheme-directory " + self._primer_scheme_dir + " --read-file  ./" + self._job_name + "_fastq_pass-" + barcode + ".fastq --fast5-directory " + self._input_folder + "/fast5_pass --sequencing-summary " + self._input_folder + "/*sequencing_summary*.txt " + self._primer_scheme + " " + self._job_name + "_" + barcode + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
-                        #output goes into current directory so this moves all output files to correct folder
-                        minion_cmd = minion_cmd + "; mv ./" + self._job_name + "_" + barcode + "* " + dir_path
-        #minion_cmd = minion_cmd + "; mv ./" + self._job_name + "* " + self._output_folder + "; \necho 'Job: " + self._job_name + " is finished running :D'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
+                        minion_cmd = minion_cmd + "; cd " + dir_path + "; artic minion --normalise " + self._normalise + " --threads " + self._num_threads + " --scheme-directory " + self._primer_scheme_dir + " --read-file  ./" + self._job_name + "_fastq_pass-" + barcode + ".fastq --fast5-directory " + self._input_folder + "/fast5_pass --sequencing-summary " + self._input_folder + "/*sequencing_summary*.txt " + self._primer_scheme + " " + self._job_name + "_" + barcode + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
+        
         minion_cmd = minion_cmd + "; \necho 'Job: " + self._job_name + " is finished running :D'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
         #change directory into output folder at the start
         minion_cmd = "cd " + self._output_folder + "; " + minion_cmd
