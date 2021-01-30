4a88c428bbc8a2de5499b8a00a403e02ea54a154
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-01-30 23:22:35 +1100

    improved the bash script

--- run_interartic.sh
+++ run_interartic.sh
@@ -1,10 +1,32 @@
 #!/bin/bash -i
 
-cd interARTIC
-( bash run-redis.sh ) &
+trap cleanup SIGINT
+
+list_descendants ()
+{
+	local children=$(ps -o pid= --ppid "$1")
+	for pid in $children
+	do
+		list_descendants "$pid"
+	done
+	echo "$children"
+}
+
+cleanup() {
+	echo "Killing all processes."
+	kill $(list_descendants $$) &> /dev/null
+}
+
+echo "Starting redis server. Log location: ./redis.log"
+( bash run-redis.sh &> redis.log ) &
 sleep 1
-( python3 main.py ) &
+echo "Starting interartic. Log location: ./interartic.log"
+( python3 main.py &> interartic.log ) &
 sleep 1
-( conda activate artic-ncov2019; celery worker -A main.celery --concurrency=1 --loglevel=info ) &
+echo "Starting celery. Log location: ./celery.log"
+( conda activate artic-ncov2019 ; celery worker -A main.celery --concurrency=1 --loglevel=info &> celery.log ) &
 sleep 1
+echo "Visit http://127.0.0.1:5000 now"
+echo "Press ctrl+c to terminate interartic"
 wait
+
