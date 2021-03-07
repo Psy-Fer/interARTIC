df6ee545cfa35901d68a587ab5e6a1f105cfe28c
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-03-08 00:14:45 +1100

    improve scripts

--- run_dev.sh
+++ run_dev.sh
@@ -17,22 +17,16 @@ cleanup() {
 	kill $(list_descendants $$) &> /dev/null
 }
 
-die() {
-	echo "$1" >&2
-	echo
-	cleanup
-	exit 1
-}
-
 echo "Starting redis server. Log location: ./redis.log"
-( bin/redis-server &> redis.log || die "Launching redis server failed. See redis.log" ) &
+( bash run-redis.sh &> redis.log ) &
 sleep 1
 echo "Starting interartic. Log location: ./interartic.log"
-( bin/python3.7 main.py &>  interartic.log || die "Launching interartic failed. See interartic.log") &
+( python3 main.py &> interartic.log ) &
 sleep 1
 echo "Starting celery. Log location: ./celery.log"
-( export PATH=`pwd`/artic_bin/bin:$PATH; export LD_LIBRARY_PATH=`pwd`/artic_bin/lib/:$LD_LIBRARY_PATH; bin/python3.7m bin/celery worker -A main.celery --concurrency=1 --loglevel=info &> celery.log || die "Launching celery failed. See celery.log" ) &
+( conda activate artic-ncov2019 ; celery worker -A main.celery --concurrency=1 --loglevel=info &> celery.log ) &
 sleep 1
 echo "Visit http://127.0.0.1:5000 now"
 echo "Press ctrl+c to terminate interartic"
 wait
+
---
+++ run_old.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then
+    echo "Usage: bash run.sh <terminal>"
+    echo "Options: xterm, macos, konsole"
+    exit 1
+fi
+
+script_full_path=$(pwd)
+term=$1
+
+if [ $term == "xterm" ]
+then
+    xterm -title "Redis Server" -e "bash $script_full_path/run-redis.sh" 
+    xterm -title "Celery" -e "cd $script_full_path; conda activate artic-ncov2019;celery worker -A main.celery --concurrency=1 --loglevel=info" 
+    xterm -title "Web App" -e "python3 $script_full_path/main.py"
+elif [ $term == "macos" ]
+then
+    osascript -e 'tell application "Terminal" to do script "bash '$script_full_path'/run-redis.sh"' 
+    osascript -e 'tell application "Terminal" to do script "cd '$script_full_path';conda activate artic-ncov2019;celery worker -A main.celery --concurrency=1 --loglevel=info"' 
+    osascript -e 'tell application "Terminal" to do script "python3 '$script_full_path'/main.py"' 
+elif [ $term == "konsole" ]
+then
+    konsole -e "bash $script_full_path/run-redis.sh"
+    konsole -e "cd $script_full_path; conda activate artic-ncov2019;celery worker -A main.celery --concurrency=1 --loglevel=info" 
+    konsole -e "python3 $script_full_path/main.py"
+fi 
+
