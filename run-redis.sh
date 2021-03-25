411d870258bcf63e258c2f078114b4df6c8f2d1f
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-03-25 22:01:44 +1100

    fix the mistake in run-redis

--- run-redis.sh
+++ run-redis.sh
@@ -6,8 +6,8 @@ die() {
 	exit 1
 }
 
-if [ ! -e redis-stable/src/redis-server ]; then
-    rm -rf redis-stable redis-6.0.12.tar.gz
+if [ ! -e redis-6.0.12/src/redis-server ]; then
+    rm -rf redis-6.0.12 redis-6.0.12.tar.gz
     wget https://download.redis.io/releases/redis-6.0.12.tar.gz || die "Downloading failed"
     tar xf redis-6.0.12.tar.gz || die "Extracting failed"
     rm redis-6.0.12.tar.gz
