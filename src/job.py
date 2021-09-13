8f7538864ceb3d59405d860f081e9827f43dd9ca
Psy-Fer <fergo1988@gmail.com>
2021-09-13 16:16:28 +1000

    fix depths, needs to be before creation

--- src/job.py
+++ src/job.py
@@ -428,38 +428,31 @@ class Job:
             # sys.stderr.write("virus: ")
             # sys.stderr.write(virus)
             # sys.stderr.write("\n")
-            depth1 = ""
-            depth2 = ""
-            vcf_file = ""
+            # depth1 = ""
+            # depth2 = ""
+            # vcf_file = ""
             file_start = self._job_name
-            for (dirpath, dirnames, filenames) in os.walk(dir_path):
-                for name in filenames:
-                    sys.stderr.write(name)
-                    sys.stderr.write("\n")
-                    if fnmatch.fnmatch(name, '*1.depths'):
-                        depth1 = os.path.join(dirpath, name)
-                        sys.stderr.write("Found depth1!!!")
-                        sys.stderr.write("\n")
-                        sys.stderr.write(depth1)
-                        sys.stderr.write("\n")
-                    if fnmatch.fnmatch(name, '*2.depths'):
-                        depth2 = os.path.join(dirpath, name)
-                        sys.stderr.write("Found depth2!!!")
-                        sys.stderr.write("\n")
-                        sys.stderr.write(depth2)
-                        sys.stderr.write("\n")
-            # depth1 = "{}/{}.coverage_mask.txt.{}_1.depths".format(dir_path, file_start, virus)
-            # depth2 = "{}/{}.coverage_mask.txt.{}_2.depths".format(dir_path, file_start, virus)
+            # for (dirpath, dirnames, filenames) in os.walk(dir_path):
+            #     for name in filenames:
+            #         sys.stderr.write(name)
+            #         sys.stderr.write("\n")
+            #         if fnmatch.fnmatch(name, '*1.depths'):
+            #             depth1 = os.path.join(dirpath, name)
+            #             sys.stderr.write("Found depth1!!!")
+            #             sys.stderr.write("\n")
+            #             sys.stderr.write(depth1)
+            #             sys.stderr.write("\n")
+            #         if fnmatch.fnmatch(name, '*2.depths'):
+            #             depth2 = os.path.join(dirpath, name)
+            #             sys.stderr.write("Found depth2!!!")
+            #             sys.stderr.write("\n")
+            #             sys.stderr.write(depth2)
+            #             sys.stderr.write("\n")
+            depth1 = "{}/{}.coverage_mask.txt*1.depths".format(dir_path, file_start)
+            depth2 = "{}/{}.coverage_mask.txt*2.depths".format(dir_path, file_start)
             vcf_file = "{}/{}.pass.vcf.gz".format(dir_path, file_start)
-            if len(depth1) < 1:
-                plot_cmd = plot_cmd + " ;echo 'ERROR: depth1 file not found! Plots not generated'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
-            if len(depth2) < 1:
-                plot_cmd = plot_cmd + " ;echo 'ERROR: depth2 file not found! Plots not generated'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
-            if len(vcf_file) < 1:
-                plot_cmd = plot_cmd + " ;echo 'ERROR: vcf_file file not found! Plots not generated'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
 
-            if depth1 and depth2 and vcf_file:
-                plot_cmd = plot_cmd + "; covarPlots.py -v {} -d1 {} -d2 {} -b {}".format(vcf_file, depth1, depth2, bed_file) + " >> " + self._output_folder + "/all_cmds_log.txt 2>>" + self._output_folder + "/all_cmds_log.txt"
+            plot_cmd = plot_cmd + "; covarPlots.py -v {} -d1 {} -d2 {} -b {}".format(vcf_file, depth1, depth2, bed_file) + " >> " + self._output_folder + "/all_cmds_log.txt 2>>" + self._output_folder + "/all_cmds_log.txt"
             # "python3 plots.py -v {} -d1 {} -d2 {} -b {}".format(vcf_file, depth1, depth2, bed_file)
 
         if self._num_samples == "multiple":
@@ -501,33 +494,26 @@ class Job:
                     # sys.stderr.write(sample_name+"\n")
                     #create directory for barcode with naming system
                     dir_path = self._output_folder + "/" + self._primer_type + "_" + sample_name + "_" + self._run_name + "_" + self._job_name + "_" + barcode + "_" + self._pipeline
-                    sys.stderr.write(dir_path)
-                    sys.stderr.write("\n")
-                    for (dirpath, dirnames, filenames) in os.walk(dir_path):
-                        for name in filenames:
-                            sys.stderr.write(name)
-                            sys.stderr.write("\n")
-                            if fnmatch.fnmatch(name, '*1.depths'):
-                                depth1 = os.path.join(dirpath, name)
-                                sys.stderr.write(depth1)
-                                sys.stderr.write("\n")
-                            if fnmatch.fnmatch(name, '*2.depths'):
-                                depth2 = os.path.join(dirpath, name)
-                                sys.stderr.write(depth2)
-                                sys.stderr.write("\n")
+                    # sys.stderr.write(dir_path)
+                    # sys.stderr.write("\n")
+                    # for (dirpath, dirnames, filenames) in os.walk(dir_path):
+                    #     for name in filenames:
+                    #         sys.stderr.write(name)
+                    #         sys.stderr.write("\n")
+                    #         if fnmatch.fnmatch(name, '*1.depths'):
+                    #             depth1 = os.path.join(dirpath, name)
+                    #             sys.stderr.write(depth1)
+                    #             sys.stderr.write("\n")
+                    #         if fnmatch.fnmatch(name, '*2.depths'):
+                    #             depth2 = os.path.join(dirpath, name)
+                    #             sys.stderr.write(depth2)
+                    #             sys.stderr.write("\n")
                     file_start = self._job_name + "_" + sample_name + "_" + barcode
-                    # depth1 = "{}/{}.coverage_mask.txt.{}_1.depths".format(dir_path, file_start, virus)
-                    # depth2 = "{}/{}.coverage_mask.txt.{}_2.depths".format(dir_path, file_start, virus)
+                    depth1 = "{}/{}.coverage_mask.txt*1.depths".format(dir_path, file_start)
+                    depth2 = "{}/{}.coverage_mask.txt*2.depths".format(dir_path, file_start)
                     vcf_file = "{}/{}.pass.vcf.gz".format(dir_path, file_start)
-                    if len(depth1) < 1:
-                        plot_cmd = plot_cmd + " ;echo 'ERROR: depth1 file not found! Plots not generated'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
-                    if len(depth2) < 1:
-                        plot_cmd = plot_cmd + " ;echo 'ERROR: depth2 file not found! Plots not generated'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
-                    if len(vcf_file) < 1:
-                        plot_cmd = plot_cmd + " ;echo 'ERROR: vcf_file file not found! Plots not generated'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
 
-                    if depth1 and depth2 and vcf_file:
-                        plot_cmd = plot_cmd + "; covarPlots.py -v {} -d1 {} -d2 {} -b {}".format(vcf_file, depth1, depth2, bed_file) + " >> " + self._output_folder + "/all_cmds_log.txt 2>>" + self._output_folder + "/all_cmds_log.txt"
+                    plot_cmd = plot_cmd + "; covarPlots.py -v {} -d1 {} -d2 {} -b {}".format(vcf_file, depth1, depth2, bed_file) + " >> " + self._output_folder + "/all_cmds_log.txt 2>>" + self._output_folder + "/all_cmds_log.txt"
 
         #change directory into output folder
         plot_cmd = plot_cmd + "; \necho 'Job: " + self._job_name + " is finished running :D'" + " >> " + self._output_folder + "/all_cmds_log.txt 2>> " + self._output_folder + "/all_cmds_log.txt"
