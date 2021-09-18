0bb02a4f6b59af1514f4be14a476f0b22a6dafa6
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-09-18 18:37:35 +1000

    update workflow for arm and mac

--- test/update.sh
+++ test/update.sh
@@ -15,9 +15,7 @@ set -x
 VERSION_NEW=0.4.1
 # VERSION_NEW=`git describe --tags` 
 
-wget ${LINK} -O interartic_bin.tar.gz
-
-#cp ~/interartic_bin.tar.gz .
+wget ${LINK} -O interartic_bin.tar.gz -q 
 tar xf interartic_bin.tar.gz
 rm interartic_bin.tar.gz
 rm -rf interartic_bin/templates interartic_bin/scripts interartic_bin/static interartic_bin/src interartic_bin/primer-schemes interartic_bin/*.sh interartic_bin/*.py interartic_bin/LICENSE.interartic.txt
@@ -25,14 +23,15 @@ git clone https://github.com/Psy-Fer/interARTIC.git
 mv interARTIC/templates interARTIC/scripts interARTIC/static interARTIC/src interARTIC/primer-schemes interARTIC/main.py interARTIC/config.init interartic_bin/
 
 if [ "${OS}" = macos ]; then
-	mv interARTIC/run.sh interartic_bin/run.sh
-else
 	mv interARTIC/run_mac.sh interartic_bin/run.sh
+else
+	mv interARTIC/run.sh interartic_bin/run.sh
 fi
 
 mv interARTIC/LICENSE interartic_bin/LICENSE.interartic.txt
 rm -rf interARTIC
 
 tar zcf interartic-v${VERSION_NEW}-${OS}-${ARCH}-binaries.tar.gz interartic_bin
-#rm -r interartic_bin
-#mv interartic_bin.tar.gz ~/interartic_bin.tar.gz
+rm -rf interartic_bin
+
+exit 0
